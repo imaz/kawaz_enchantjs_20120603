9c49cf45ea1de8b16fc06fb30be9ea20b462b4e1
IMAZU Asami <neko.daisuki.070109@gmail.com>
2012-06-03 17:57:56 +0900

    左右に動く様にした

--- src/main.js
+++ src/main.js
@@ -9,11 +9,25 @@ window.onload = function(){
     player.image = game.assets['./resources/images/player.png'];
     player.frame = [0,0,1,1,2,2,3,3];
     player.rotate(-5);
-    player.scale(1.2,1);
+    player.scale(1,1);
     //enchant.Entity
     player.backgroundColor = 'red';
     //enchant.Node
     player.moveBy(30,59);
+
+    //event
+    player.addEventListener('enterframe',function(){
+      this.speed = 0;
+      if(game.input.left){
+        this.scaleX = 1;
+        this.speed = -5;
+      }else if(game.input.right){
+        this.scaleX = -1;
+        this.speed = 5;
+      }
+      this.x += this.speed;
+    });
+
     game.rootScene.addChild(player);
   };
   game.start();
