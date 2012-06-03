36b6531955d92d46530cbaea3bf58546d9fbbf1b
IMAZU Asami <neko.daisuki.070109@gmail.com>
2012-06-03 17:13:04 +0900

    倍率と位置を調整 enchant.Node

--- src/main.js
+++ src/main.js
@@ -4,10 +4,14 @@ window.onload = function(){
   var game = new Game(320,320);
   game.preload('./resources/images/player.png');
   game.onload = function(){
+    //enchant.Sprite
     var player = new Sprite(32,32);
     player.image = game.assets['./resources/images/player.png'];
     player.frame = [0,0,1,1,2,2,3,3];
-    player.rotation = 33;
+    player.rotate(-5);
+    player.scale(1.2,1);
+    //enchant.Node
+    player.moveBy(30,59);
     game.rootScene.addChild(player);
   };
   game.start();
